cb94d741eb6e1f0ca33f8b4c09c44eedef59d13b
carlosp420 <mycalesis@gmail.com>
2015-08-31 11:30:59 +0300

    refactoring

--- elum/api.py
+++ elum/api.py
@@ -1,33 +1,6 @@
 from Bio import Entrez
 
 
-def convert_metadata_to_line(metadata):
-    out = ''
-    try:
-        out += metadata['Title']
-    except KeyError:
-        out += '\t'
-
-    try:
-        out += str(metadata['Length'])
-    except KeyError:
-        out += '\t'
-
-    return out
-
-
-def query_ncbi(gi):
-    """
-
-    :param gi: NCBI identifier
-    :return: metadata as dict
-    """
-    handle = Entrez.esummary(db="nucleotide", id=gi, retmode="text")
-    record = Entrez.read(handle)
-    handle.close()
-    return record[0]
-
-
 def complete_me(content_as_list, email):
     """
     Add metadata to the blast output file. Metadata is obtained by querying the
@@ -47,11 +20,52 @@ def complete_me(content_as_list, email):
             append(line + '\t' + 'Title\tGeneLength')
             continue
 
-        line_as_fields = line.split('\t')
-        identifier_column = line_as_fields[1].split('|')
-        gi = identifier_column[1]
-        metadata = query_ncbi(gi)
-        line_complement = convert_metadata_to_line(metadata)
+        line_complement = _get_metadata_as_string(line)
+
         append(line + '\t' + line_complement)
-        break
-    print('\n'.join(output))
+    return '\n'.join(output)
+
+
+def _get_metadata_as_string(line):
+    """
+    Takes a line of the blast output CSV file, extracts the GI (genbank idendifier)
+    and returns the extra metadata as string.
+
+    :param line:
+    :return:
+    """
+    line_as_fields = line.split('\t')
+    identifier_column = line_as_fields[1].split('|')
+
+    gi = identifier_column[1]
+    metadata = _query_ncbi(gi)
+    line_complement = _convert_metadata_to_line(metadata)
+
+    return line_complement
+
+
+def _query_ncbi(gi):
+    """
+
+    :param gi: NCBI identifier
+    :return: metadata as dict
+    """
+    handle = Entrez.esummary(db="nucleotide", id=gi, retmode="text")
+    record = Entrez.read(handle)
+    handle.close()
+    return record[0]
+
+
+def _convert_metadata_to_line(metadata):
+    out = ''
+    try:
+        out += metadata['Title']
+    except KeyError:
+        out += '\t'
+
+    try:
+        out += str(metadata['Length'])
+    except KeyError:
+        out += '\t'
+
+    return out
